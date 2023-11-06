3a5f09ec3d2ccea26922f4cb1747695bfbcd36e5
Kalong98 <68122498+Kalong98@users.noreply.github.com>
2023-11-06 17:09:17 +0100

    I2C build

--- projects/I2C/i2c.cpp
+++ projects/I2C/i2c.cpp
@@ -3,43 +3,28 @@
 #include <thread>
 #include <chrono>
 
-I2CSimulation::I2CSimulation(uint8_t address):
-address(address),
+
+
+I2CSimulation::I2CSimulation(LM75 lm75, SHT35 sht35):
+lm75 (lm75),
+sht35 (sht35),
 dataRegister(0),
 data(0),
 currentDevice(0)
 {}
 
 void I2CSimulation::write(uint8_t registerAddress, uint8_t value) {
-    if (currentDevice == address) {
+    if (currentDevice == LM75_ADRESS) {
         if (registerAddress == LM75_TEMP_REGISTER) {
-        // Wire.beginTransmission(address);
-        // Wire.write(LM75_TEMP_REGISTER);
-        // Wire.endTransmission();
-
-        // Wire.requestFrom(address, 2);
-        // if (Wire.available() >= 2) {
-        // int raw_temperature = Wire.read() << 8 | Wire.read();
-        // return (double(raw_temperature >> 7) * 0.5);
-        // }
-        // return -1.0; // Error reading temperature
             dataRegister = value;
         }
         // Add more conditionals for other device registers as needed
     }
-
-    // SHT35 data:
-    // 	u8 data[6]={0};
-	// u16 temp_hex=0,hum_hex=0;
-	// CHECK_RESULT(ret,send_command(cfg_cmd));
-	// CHECK_RESULT(ret,read_bytes(data,sizeof(data),CLK_STRCH_STAT));
-
-	// temp_hex=(data[0] << 8) | data[1];
-	// hum_hex =(data[3] << 8) | data[4];
-
-	// *temp =get_temp(temp_hex);
-	// *hum=get_hum(hum_hex);
-
+    else if (currentDevice == SHT35_ADRESS){}
+    {
+        /* code */
+    }
+    
 }
 
 void I2CSimulation::request(uint8_t numBytes) {
--- projects/I2C/i2c.hpp
+++ projects/I2C/i2c.hpp
@@ -2,16 +2,21 @@
 #define I2C_SIMULATION_H
 
 #include <cstdint>
+#include "projects\LM75\lm75.hpp"
+#include "projects\SHT35\sht35.hpp"
+
+#define LM75_ADRESS 0x48
+#define SHT35_ADRESS 0x44
 
 class I2CSimulation {
 private:
-    uint8_t address;
+    LM75 lm75;
+    SHT35 sht35;
     uint8_t dataRegister;
     uint16_t data;
     uint8_t currentDevice;
 public:
-    I2CSimulation(uint8_t address);
-
+    I2CSimulation::I2CSimulation(LM75 lm75, SHT35 sht35);
     void write(uint8_t registerAddress, uint8_t value);
     void request(uint8_t numBytes);
     uint16_t read();
